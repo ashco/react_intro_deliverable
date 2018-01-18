bc72f57c9e167291b1b9eb7f3adf07572e74cd46
ashco <ashchristie@gmail.com>
2018-01-18 09:40:47 -0800

    mvp

---
+++ src/Author.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//inherit properties from component, plus can do other things like..
+class Author extends Component {
+  render() {
+		//Return - what is coming out of render function
+		return (
+			<div>Author component: TBD</div>
+
+		);
+	}
+}
+
+//works like exports in Node.js
+export default Author;
